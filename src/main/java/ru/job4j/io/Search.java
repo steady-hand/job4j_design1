33d8198edbdec9460084f4f99637ee0d7b74c5c1
vg52 <v.gabrusenok10@gmail.com>
2026-06-08 16:58:10 +0300

    12. Валидация параметров запуска [#246865 #66619]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,31 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate (String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        String ext = args[1];
+        if (!Files.exists(start)) {
+            throw new IllegalArgumentException(String.format("Not exist %s", start.toAbsolutePath()));
+        }
+        if (!Files.isDirectory(start)) {
+            throw new IllegalArgumentException(String.format("Not directory %s", start.toAbsolutePath()));
+        }
+        if (ext.isEmpty()) {
+            throw new IllegalArgumentException("Extension cannot be empty");
+        }
     }
 }
